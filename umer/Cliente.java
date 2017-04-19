75a0f04ab17fc0822cadcfc8614efd71bd7a23c8
Sérgio Jorge <stoj97@gmail.com>
2017-04-19 19:37:13 +0100

    Subclasse Cliente e Subclasse Motorista

--- umer/Cliente.java
+++ umer/Cliente.java
@@ -1,10 +1,13 @@
-/*
-public class Cliente extends Utilizador
-	public Cliente(String _nome, String _cpf, Date _data) {
-		super(_nome, _cpf, _data);
-	}
-	public String matricula;
 
+public class Cliente extends Utilizador{
+    
+	public Cliente(String newEmail, String newNome, String newPassword, String newMorada, String newDataNascimento){
+		          super(newEmail, newNome, newPassword, newMorada, newDataNascimento);
+	}
+	public String historicodeviagens;
+	//O cliente está sempre numa determinada localização (expressa em x e y, isto é, num espaço 2D) e 
+	//escolhe um táxi especíco ou então solicita o táxi mais perto que esteja disponível. 
+	//O cliente tem também uma relação de todas as viagens que fez, com toda a informação relativa à viagem.
+	//TEMOS DE VER COMO DEFINIMOS A localizacao / ponto
 
 } 
-*/
--- umer/Motorista.java
+++ umer/Motorista.java
@@ -1,3 +1,16 @@
-/*
-
-*/
+public class Motorista extends Utilizador{
+    
+    public Motorista(String newEmail, String newNome, String newPassword, String newMorada, String newDataNascimento){
+                  super(newEmail, newNome, newPassword, newMorada, newDataNascimento);
+    }
+    // grau de cumprimento de horário estabelecido com o cliente, dado por um factor entre 0 e 100
+    public int cumprimento;
+    // classificação do motorista, dado numa escala de 0 a 100, calculada com base na classificação dada pelo cliente no final da viagem;
+    public int classificacao;
+    // historico de viagens array?
+    
+    // kms feitos na empresa
+    public int kms;
+    // informacao se está disponivel ou não, a trabalhar ou não
+    public boolean disponibilidade;
+} 
