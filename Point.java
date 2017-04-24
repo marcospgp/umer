655a8b968de8427b5045ce6cb8b8d34f872844da
Marcos Pereira <marcospereiradev@gmail.com>
2017-04-24 15:40:56 +0100

    Adicionar classe Point e enum VehicleType

---
+++ Point.java
@@ -0,0 +1,71 @@
+
+/**
+ * Classe genérica que representa um ponto.
+ * Pode ser instanciada com valores de qualquer tipo que
+ * herde to tipo Number. No entanto, para todos os cálculos efetuados
+ * pelos métodos desta classe, os valores são convertidos para double.
+ */
+public class Point <T extends Number> {
+
+    private T x;
+    private T y;
+
+    /**
+     * Constructor for objects of class Point
+     */
+    public Point(T x, T y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    /**
+     * Returns the X coordinate of the point
+     *
+     * @return     the x coordinate
+     */
+    public T getX() {
+        return this.x;
+    }
+
+    /**
+     * Returns the Y coordinate of the point
+     *
+     * @return     the y coordinate
+     */
+    public T getY() {
+        return this.y;
+    }
+
+    /**
+     * Sets the X coordinate of the point
+     *
+     * @return     the x coordinate
+     */
+    public void setX(T x) {
+        this.x = x;
+    }
+
+    /**
+     * Sets the Y coordinate of the point
+     *
+     * @return     the y coordinate
+     */
+    public void setY(T y) {
+        this.y = y;
+    }
+
+    /**
+     * Returns the distance from this point to another
+     * The double value of both points is used for the calculation.
+     *
+     * @param  b   point to calculate the distance to
+     * @return     the distance between this point and point b
+     */
+    public double distanceTo(Point b) {
+
+        double dX = Math.abs( b.getX().doubleValue() - this.x.doubleValue() );
+        double dY = Math.abs( b.getY().doubleValue() - this.y.doubleValue() );
+
+        return Math.sqrt( Math.pow(dX, 2) + Math.pow(dY, 2) );
+    }
+}
