e463a79923df2c5523f16670a88982bb924eb172
Sérgio Jorge <stoj97@gmail.com>
2017-04-26 17:32:40 +0100

    Load/Save done

--- IO.java
+++ IO.java
@@ -8,29 +8,38 @@ import java.io.IOException;
 import java.util.List;
 import java.io.FileNotFoundException;
 
+/* ESTA CLASSE ESTÁ APENAS PREPARADA PARA 2 ARRAYLISTS */
+
 public class IO
 {
    public static void main(String [] args){
-       ArrayList<String> al = new ArrayList<String>();
-       al.add("Sergio");
-       al.add("Vitor");
-       al.add("Marcos");
-       Write(al);
-       ArrayList<String> arraylist = new ArrayList<String>();
-       Read(arraylist);
+       ArrayList<String> a1 = new ArrayList<String>();
+       a1.add("Sergio1");
+       a1.add("Vitor1");
+       a1.add("Marcos1");
+       ArrayList<String> a2 = new ArrayList<String>();
+       a2.add("Sergio2");
+       a2.add("Vitor2");
+       a2.add("Marcos2");
+       Write(a1,1);
+       Write(a2,2);
+       ArrayList<String> arraylist1 = new ArrayList<String>();
+       ArrayList<String> arraylist2 = new ArrayList<String>();
+       Read(arraylist1,2);
     }
     
-   public static void Write(ArrayList<String> al) {
+   public static void Write(ArrayList<String> l, int ID) {
         // creating output stream variables
         FileOutputStream fileoutput = null;
         ObjectOutputStream objectoutput = null;
         try {
             // writing data
-            fileoutput = new FileOutputStream("data.txt");
+            if (ID==1) fileoutput = new FileOutputStream("data1.txt");
+            if (ID==2) fileoutput = new FileOutputStream("data2.txt");
             // converting object to binary
             objectoutput = new ObjectOutputStream(fileoutput);
             // writing ArrayList to stream
-            objectoutput.writeObject(al);
+            objectoutput.writeObject(l);
             objectoutput.flush();
             objectoutput.close();
         } 
@@ -44,13 +53,14 @@ public class IO
     }
     
 
-    public static void Read(ArrayList<String> l) {
+    public static void Read(ArrayList<String> l, int ID) {
          // creating input stream variables
         FileInputStream fileinput = null;
         ObjectInputStream objectinput = null;
         try {
             // reading data
-            fileinput = new FileInputStream("data.txt");
+            if (ID==1) fileinput = new FileInputStream("data1.txt");
+            if (ID==2) fileinput = new FileInputStream("data2.txt");
             // converting data to object
             objectinput = new ObjectInputStream(fileinput);
             // reading object's value and checking if input object is an Arraylist
